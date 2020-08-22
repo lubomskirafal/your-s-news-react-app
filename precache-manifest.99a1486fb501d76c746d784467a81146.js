427c788ed79a5a82260e2c2052389f82aac48258
lubomskirafal <lubomski.rafal@gmail.com>
2020-08-22 19:08:05 +0200

    Updates

---
+++ precache-manifest.99a1486fb501d76c746d784467a81146.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f832ba822728902b7c81de3f19233db1",
+    "url": "/your-s-news-react-app/index.html"
+  },
+  {
+    "revision": "a2488d1dae6ad8c1cc3a",
+    "url": "/your-s-news-react-app/static/css/main.09f6fa58.chunk.css"
+  },
+  {
+    "revision": "a999fd66b31b454f70bf",
+    "url": "/your-s-news-react-app/static/js/2.946406ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/your-s-news-react-app/static/js/2.946406ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2488d1dae6ad8c1cc3a",
+    "url": "/your-s-news-react-app/static/js/main.aaf63779.chunk.js"
+  },
+  {
+    "revision": "73e2e07a543c73d9a5a7",
+    "url": "/your-s-news-react-app/static/js/runtime-main.15905b96.js"
+  }
+]);
